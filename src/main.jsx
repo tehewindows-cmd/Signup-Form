038230904a645755d61f98f100e4abfb6de22eb3
Taha <tehewindows@gmail.com>
2026-09-02 15:49:18 +0330

    !

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './WeatherApp/index.css'
-import App from './WeatherApp/App.jsx'
+import './Form/indexFa.css'
+import App from './Form/App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
